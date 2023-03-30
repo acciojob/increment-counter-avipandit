1e6353658b993b0eca07f45a04bbe0beaa0b658d
AVINASH KUMAR <65335071+avipandit@users.noreply.github.com>
2023-03-30 21:01:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+const incrementCount = document.getElementById("incrementBtn");
+
+const totalCount = document.getElementById("counter");
+
+var count = 0;
+
+// Display initial count value
+totalCount.innerHTML = count;
+
+// Function to increment count
+const handleIncrement = () => {
+  count++;
+  totalCount.innerHTML = count;
+};
+incrementBtn.addEventListener("click", handleIncrement);
